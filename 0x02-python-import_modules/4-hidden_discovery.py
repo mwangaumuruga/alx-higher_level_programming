74d2e3a7492ca42c0306a78bb502170ecaa239a3
CODEKARTEL <codekartel@localhost.localdomain>
2024-03-20 15:53:28 +0300

    firstCommit

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
+    """Print all hidden directories"""
     import hidden_4
-    for s in dir(hidden_4):
-        if s[:2] != "__":
-            print("{:s}".format(s))
+
+    for i in dir(hidden_4):
+        if i[:2] != "__":
+            print(i)
